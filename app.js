e5d34043b3228c8f85bd557d01cbe6673939ee39
suryasengautam <94212580+suryasengautam@users.noreply.github.com>
2021-12-12 02:07:57 +0530

    Add files via upload
    yoda split sentence

---
+++ app.js
@@ -0,0 +1,32 @@
+var btntranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverUrl = "https://api.funtranslations.com/translate/"
+
+
+function getTranslationURL(serverUrl) {
+    return serverUrl + "?" + "text=" + txtInput.value
+    
+}
+
+
+function errorHandler(error) {
+    console.log("error",error);
+    alert("something went wrong with the server. please try after some time")
+}
+
+function clickHandler() {
+    // calloing server for processing
+    fetch(getTranslationURL(serverUrl))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText=translatedText;
+             })
+        .catch(errorHandler)
+    
+};
+
+
+btntranslate.addEventListener("click",clickHandler)
