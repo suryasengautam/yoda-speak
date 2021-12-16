63560b6a5edef166bd5ada3dabbb06d89306a29f
suryasengautam <94212580+suryasengautam@users.noreply.github.com>
2021-12-16 07:27:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var btntranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverUrl = "https://api.funtranslations.com/translate/"
+var serverUrl = "https://api.funtranslations.com/translate/yoda.json"
 
 
 function getTranslationURL(serverUrl) {
